0f83470214a2026592dd32f6968a17fd14f398e1
Kawaichi0228 <99755034+Kawaichi0228@users.noreply.github.com>
2022-07-17 21:49:58 +0900

    refactor

--- src/components/molecules/HamburgerMenu.vue
+++ src/components/molecules/HamburgerMenu.vue
@@ -1,45 +1,26 @@
+<script setup lang="ts">
+import { reactive, UnwrapNestedRefs } from "vue";
+
+type Data = {
+  isClick: boolean;
+}
+const state: UnwrapNestedRefs<Data> = reactive<Data>({
+  isClick: false,
+});
+</script>
+
+
 <template>
   <!--ハンバーガーメニューのボタン-->
-  <div
-    class="hamburger-button"
-    @click="
-      state.isClick = !state.isClick;
-      $emit('hamburger-btn-click', state.isClick);
-    "
-  >
+  <div class="hamburger-button" @click="
+  state.isClick = !state.isClick;
+$emit('hamburger-btn-click', state.isClick);
+  ">
     <span class="line line_01" :class="{ btn_line01: state.isClick }"></span>
     <span class="line line_02" :class="{ btn_line02: state.isClick }"></span>
     <span class="line line_03" :class="{ btn_line03: state.isClick }"></span>
   </div>
 </template>
-
-<script lang="ts">
-import { defineComponent, reactive } from "vue";
-
-// 変数の型定義
-interface Data {
-  isClick: boolean;
-}
-
-export default defineComponent({
-  setup() {
-    // ref(=data)の初期値定義
-    //   MEMO: 前リターン -> ref<type || interface> & 後リターン
-    //     data: (): Data => {
-    //     return {
-
-    // interfaceの場合は`reactive`を使う
-    const state = reactive<Data>({
-      isClick: false,
-    });
-    // 単体のdataの場合は`ref`を使う
-    //const isClick = ref<boolean>(false);
-    return {
-      state,
-    };
-  },
-});
-</script>
 
 <style lang="scss" scoped>
 // ハンバーガーボタン全体
@@ -68,30 +49,36 @@ export default defineComponent({
 
     text-align: center;
   }
+
   .line_01 {
     // 1本目
     top: 16px;
     transition: var(--p_fadeinTime) ease;
   }
+
   .line_02 {
     // 2本目
     top: 26px;
     transition: var(--p_fadeinTime) ease;
   }
+
   .line_03 {
     // 3本目
     top: 36px;
     transition: var(--p_fadeinTime) ease;
   }
 }
+
 .btn_line01 {
   transform: translateY(10px) rotate(-45deg);
   transition: var(--p_fadeinTime) ease;
 }
+
 .btn_line02 {
   transition: var(--p_fadeinTime) ease;
   opacity: 0;
 }
+
 .btn_line03 {
   transform: translateY(-10px) rotate(45deg);
   transition: var(--p_fadeinTime) ease;
--- src/components/organisms/HomeMainVisual.vue
+++ src/components/organisms/HomeMainVisual.vue
@@ -9,7 +9,7 @@ import gsap from "gsap";
 type Panel = {
   value: string
 };
-const panels: Ref<Array<Panel>> = ref([
+const panels: Ref<Array<Panel>> = ref<Array<Panel>>([
   { value: "Go / " },
   { value: "Gin / " },
   { value: "Python / " },
@@ -44,17 +44,11 @@ const panelsEnter = (el: any, done: any) => {
   <div class="home-main-visual pt-24 pb-60 sr__fadein-top">
     <div class="w-10/12 mx-auto">
       <div class="flex justify-center mb-2">
-        <img
-          class="home-main-visual__image"
-          src="@\assets\images\logo\logo.png"
-        />
+        <img class="home-main-visual__image" src="@\assets\images\logo\logo.png" />
       </div>
       <div class="flex justify-center mb-2">
         <h1>
-          <img
-            class="home-main-visual__title"
-            src="@\assets\images\logo\title.png"
-          />
+          <img class="home-main-visual__title" src="@\assets\images\logo\title.png" />
         </h1>
       </div>
       <div class="text-primary tracking-widest sr__fadein-bottom-delay">
@@ -62,18 +56,8 @@ const panelsEnter = (el: any, done: any) => {
       </div>
       <!-- 各言語の内容 -->
       <div class="-mt-1 leading-9">
-        <transition-group
-          tag="span"
-          appear
-          @before-enter="panelsBeforeEnter"
-          @enter="panelsEnter"
-        >
-          <span
-            class="text-sm tracking-widest"
-            v-for="(panel, index) in panels"
-            :key="panel.value"
-            :data-index="index"
-          >
+        <transition-group tag="span" appear @before-enter="panelsBeforeEnter" @enter="panelsEnter">
+          <span class="text-sm tracking-widest" v-for="(panel, index) in panels" :key="panel.value" :data-index="index">
             <span>{{ panel.value }}</span>
           </span>
         </transition-group>
@@ -83,12 +67,13 @@ const panelsEnter = (el: any, done: any) => {
 </template>
 
 <style lang="scss" scoped>
-.home-main-visual {
-}
+.home-main-visual {}
+
 .home-main-visual__image {
   max-height: 5rem;
   margin-bottom: 0.5rem;
 }
+
 .home-main-visual__title {
   max-height: 2rem;
 }
