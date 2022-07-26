3312c6a3b1537e4e4f456735839b31ac8fc0fd32
Kawaichi0228 <99755034+Kawaichi0228@users.noreply.github.com>
2022-07-27 00:49:23 +0900

    fix

--- src/components/atoms/ButtonRightAllow.vue
+++ src/components/atoms/ButtonRightAllow.vue
@@ -60,7 +60,7 @@ defineProps<Props>();
         xmlns="http://www.w3.org/2000/svg">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path>
       </svg>
-      >WebSite</span>
+      WebSite</span>
     <span class="relative invisible">WebSite</span>
   </a>
 </template>
